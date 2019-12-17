c91f284051c1b4a2b889a1fdb6a2ecddc10de31c
logomin <logominov@rambler.ru>
2019-12-17 14:27:40 +0200

    Initial commit

---
+++ src/FirstClient.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class FirstClient {
+    public static void main(String[] args) {
+        //while (true) {
+        try (Socket socketClient1 = new Socket(InetAddress.getLocalHost(), 1025)) {
+            //socketClient1.connect();
+            System.out.println("Я (Клиент 1) подключился к серверу");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socketClient1.getInputStream()));
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socketClient1.getOutputStream()));
+
+            // делаем поток для чтения с клавиатуры
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+
+            while (true) {
+
+                // делаем потоки для ввода вывода в сеть
+
+                System.out.println("Напечатай что-нибудь и нажми Enter\n");
+                String line = null;
+                line = keyboard.readLine();
+                System.out.println("Отправляем эту строку серверу");
+                bufferedWriter.write(line + "\n");
+                bufferedWriter.flush();
+                if (line.equals("quit")) break;
+                String string1 = bufferedReader.readLine();
+                System.out.println(string1);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //}
+    }
+}
